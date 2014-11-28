4ab36efe3cc64d987c3103c2ebf557f9eb60feb3
TI 6 <TI 6@TI6>
2014-11-28 15:39:39 +0530

    \\28-11-2014 3:15

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
         echo "Hello, Dilip Patel";
-        echo "How are you ?";
+        echo "How are you Today?";
         ?>
     </body>
 </html>
